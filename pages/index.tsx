ebe529cac24b2303c99c68e870cb8ce3f9f2b775
Mohamad Tarhini <mdtarhini@outlook.com>
2022-02-21 17:02:35 +0100

    updated script

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const HomePage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Home />
-      <Script id="user-way">{`(function(d){var s = d.createElement("script");s.setAttribute("data-account", "lHPKorqUyp");s.setAttribute("src", "https://cdn.userway.org/widget.js");(d.body || d.head).appendChild(s);})(document)`}</Script>
+      <Script id="user-way">{`(function(){ var s = document.createElement('script'); var h = document.querySelector('head') || document.body; s.src = 'https://acsbapp.com/apps/app/dist/js/app.js'; s.async = true; s.onload = function(){ acsbJS.init({ statementLink : '', footerHtml : '', hideMobile : false, hideTrigger : false, disableBgProcess : false, language : 'en', position : 'right', leadColor : '#146FF8', triggerColor : '#146FF8', triggerRadius : '50%', triggerPositionX : 'right', triggerPositionY : 'bottom', triggerIcon : 'people', triggerSize : 'bottom', triggerOffsetX : 20, triggerOffsetY : 20, mobile : { triggerSize : 'small', triggerPositionX : 'right', triggerPositionY : 'bottom', triggerOffsetX : 20, triggerOffsetY : 20, triggerRadius : '20' } }); }; h.appendChild(s); })();`}</Script>
     </div>
   );
 };
